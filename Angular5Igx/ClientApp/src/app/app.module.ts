eb10034fc8d896b9dba9c32cbd526ce77cc14e7d
Michel LAPLANE <michel.laplane@hotmail.com>
2018-07-18 08:24:20 +0200

    V0.5.1.0 - Adding of Infragistics component page abd date picker sample.

--- Angular5Igx/ClientApp/src/app/app.module.ts
+++ Angular5Igx/ClientApp/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import the IgxDatePickerModule that will be used 
+import { IgxDatePickerModule } from 'igniteui-angular/main';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { InfragisticsSampleComponent } from './infragisticssample/InfragisticsSample.component';
 
 @NgModule({
   declarations: [
@@ -16,16 +20,20 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    InfragisticsSampleComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    IgxDatePickerModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'infragisticssample', component: InfragisticsSampleComponent }
     ])
   ],
   providers: [],
---
+++ Angular5Igx/ClientApp/src/app/infragisticssample/infragisticssample.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'infragisticssample',
+  templateUrl: './infragisticssample.component.html'
+})
+export class InfragisticsSampleComponent
+{
+
+}
